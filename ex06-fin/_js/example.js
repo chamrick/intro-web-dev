eaf6c45017f1ef188583a14f97d461437f1a832c
Cord Hamrick <Cord.Hamrick@weather.com>
2018-06-27 00:32:50 -0400

    Duplicated completed files to -fin folders;

---
+++ ex06-fin/_js/example.js
@@ -0,0 +1,60 @@
+var handleClick;
+
+function initProductShow() {
+  var productsImgFolder = '_prod_img/';
+  var productsArray = [
+    {
+      img: (new Image()),
+      file: 'p01-glue.jpg',
+      caption: 'ACME Super Fast Glue'
+    },
+    {
+      img: (new Image()),
+      file: 'p02-wheeled-rocket.jpg',
+      caption: 'ACME Wheeled Rocket'
+    },
+    {
+      img: (new Image()),
+      file: 'p03-rocket-sled.jpg',
+      caption: 'ACME Rocket Sled'
+    },
+    {
+      img: (new Image()),
+      file: 'p04-earthquake-pills.jpg',
+      caption: 'ACME Earthquake Pills'
+    },
+    {
+      img: (new Image()),
+      file: 'p05-speed-tonic.jpg',
+      caption: 'ACME Speed Tonic'
+    },
+    {
+      img: (new Image()),
+      file: 'p06-dehydrated-boulders.jpg',
+      caption: 'ACME Dehydrated Boulders'
+    },
+    {
+      img: (new Image()),
+      file: 'p07-wrong-site.png',
+      caption: 'Dude! This is not my \'toon! I\'m on the wrong @#$%&* site!'
+    }
+  ];
+  productsArray.forEach(function(obj) {
+    obj.img.src = productsImgFolder + obj.file;
+  });
+
+  var productsShowImg = document.getElementById('productsShowImg');
+  var productsShowCaption = document.getElementById('productsShowCaption');
+  var currentDisplayIndex = -1;
+
+  var clickHandler = function() {
+    currentDisplayIndex++;
+    if (currentDisplayIndex === productsArray.length) {
+      currentDisplayIndex = 0;
+    }
+    productsShowImg.src = productsArray[currentDisplayIndex].img.src;
+    productsShowCaption.innerHTML = productsArray[currentDisplayIndex].caption;
+  }
+
+  return clickHandler;
+}
